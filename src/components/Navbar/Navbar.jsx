133777a60b23e033cafd273a1f58a5038a0e3da4
Jack Chuma <jack@pixelvault.com>
2022-08-10 16:18:49 -0400

    mobile menu fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
 
 import './Navbar.scss';
 
 const sections = ['home', 'about', 'work', 'skills', 'contact'];
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+
+  function toggleHiddenMenu(show) {
+    const menu = document.getElementById('hidden');
+    if (show) menu.style.transform = 'translateX(0)';
+    else menu.style.transform = 'translateX(101%)';
+  }
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo logo-text'>
@@ -24,22 +29,17 @@ const Navbar = () => {
         ))}
       </ul>
       <div className='app__navbar-menu'>
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {sections.map(item => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
+        <HiMenuAlt4 onClick={() => toggleHiddenMenu(true)} />
+        <div id='hidden' className='app__navbar-menu-hidden'>
+          <HiX onClick={() => toggleHiddenMenu(false)} />
+          <ul>
+            {sections.map(item => (
+              <li key={item}>
+                <a href={`#${item}`} onClick={() => toggleHiddenMenu(false)}>{item}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   )
